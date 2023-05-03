7739180286d6f48d66d0190bbe8e76b6aae7c9b7
Hieu Nguyen <hieu.nguyen188@hcmut.edu.vn>
2023-05-03 16:21:39 +0700

    feat: analytics on main screen now display lastest data instead of icon + module name

--- frontend/component/analytics.component.js
+++ frontend/component/analytics.component.js
@@ -2,10 +2,14 @@ import { View, Text, StyleSheet, Image, Alert, Pressable } from "react-native";
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
 import ModuleDetail from "../screens/room_detail.screen";
+import axios from "axios";
+import Moment from "moment";
 const AnalyticsItem = (props) => {
     const { name, image } = props;
     const navigation = useNavigation();
-
+    const [status, setStatus] = useState("F")
+    const [doorStatus, setDoorStatus] = useState("0");
+    const [doorTimestamp, setDoorTimestamp] = useState("0");
     const renderIcon = () => {
         return (
             <View
@@ -28,11 +32,46 @@ const AnalyticsItem = (props) => {
             </View>
         );
     };
-
+    const getModuleValue = async () => {
+        const result = await axios.get(
+          `https://io.adafruit.com/api/v2/khoitran1422/feeds/bbc-moisture`
+        );
+        // console.log(result.data);
+        setDoorStatus(result.data.last_value);
+        setDoorTimestamp(result.data.updated_at);
+    }
+    useEffect(() => {
+        const isMounted = true;
+        const intervalid = setInterval(() => {
+            getModuleValue();
+        }, 1000);
+        return () => {
+            clearInterval(intervalid);
+            isMounted = false;
+        };
+      }, []);
     return (
         <Pressable style={style.container} onPress={() => navigation.navigate("ROOM DETAIL")}>
-            <View>{renderIcon()}</View>
-            <Text style={style.name}>{name}</Text>
+            <View
+                style={{
+                    paddingHorizontal: 30,
+                    width: "100%",
+                    marginTop: 10,
+                }}
+            >
+                <View style={style.profileItem}>
+                    <View style={style.infoItem}>
+                        <Text style={style.titleItem}>Module Name</Text>
+                        <Text style={style.desItem}>
+                        {name}
+                        </Text>
+                    </View>
+                    <View style={style.infoItem}>
+                        <Text style={style.titleItem}>Module Status</Text>
+                        <Text style={style.desItem}>{doorStatus} at {Moment(doorTimestamp).format('HH:mm DD/MM/YYYY')}</Text>
+                    </View>
+                </View>
+            </View>
         </Pressable>
     );
 };
@@ -42,17 +81,39 @@ const style = StyleSheet.create({
         alignItems: "center",
         backgroundColor: "#E9F4FF",
         width: "35%",
-        height: 100,
+        height: 200,
         marginHorizontal: "7.5%",
         marginVertical: 20,
         borderRadius: 10,
-        paddingVertical: 25,
         paddingHorizontal: 10,
     },
     name: {
         color: "#4C51C6",
         fontWeight: "700",
         fontSize: 14,
+    },
+    infoItem: {
+        // height: 80,
+        marginTop: 15,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    titleItem: {
+        fontSize: 24,
+        fontWeight: "600",
+        letterSpacing: 1,
+        color: "#444",
+    },
+    desItem: {
+        fontSize: 20,
+        fontWeight: "600",
+        color: "#666",
+        marginTop: 10,
+    },
+    profileItem: {
+        height: 80,
+        flexDirection: "column",
+        marginBottom: 0,
     },
 });
 
